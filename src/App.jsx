bb156854142497d12215b85473a7356aa782e8fa
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-24 12:25:06 +0200

    Add routes to Product, Pricing and Homepage elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Homepage from "./Pages/Homepage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
