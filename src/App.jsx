77b314fd865b8a0c97a479ebac264873a713d46d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-24 16:36:51 +0200

    AppNav placeholder content, include AppNav component into AppLayout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./Pages/Product";
-import Pricing from "./Pages/Pricing";
-import Homepage from "./Pages/Homepage";
-import PageNotFound from "./Pages/PageNotFound";
-import AppLayout from "./Pages/AppLayout";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
