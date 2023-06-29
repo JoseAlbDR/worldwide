fe84a9f8c81b9a6a1b4f398459d243710810e89e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-29 10:24:37 +0200

    feat: extract rest of starter files into components folder

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 import { useState } from "react";
@@ -31,7 +32,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Link to="app" className="cta">
+            Login
+          </Link>
         </div>
       </form>
     </main>
