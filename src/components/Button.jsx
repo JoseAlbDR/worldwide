e29a321ff6cc46e9df09960ba517f5c5cf9fa6af
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-01 00:01:41 +0200

    style: remove random logs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,5 @@
 import styles from "./Button.module.css";
 function Button({ children, onClick, type }) {
-  console.log(onClick);
   return (
     <button className={`${styles.btn} ${styles[type]}`} onClick={onClick}>
       {children}
