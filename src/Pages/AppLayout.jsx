e16a2bc31f79707c0a8974c1ed7f0ac1c1850791
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-30 09:54:53 +0200

    Change esling config

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,6 +1,7 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
+import AppNav from "../components/AppNav";
 
 function AppLayout() {
   return (
