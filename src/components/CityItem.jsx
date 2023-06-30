7df36aa2aa5dfca9da539dcce0607144ec49ff69
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-30 11:11:40 +0200

    feat: finish CityList and CityItem components layout

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,7 +1,21 @@
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
 function CityItem({ city }) {
   return (
     <>
-      <li>{city.cityName}</li>
+      <li className={styles.cityItem}>
+        <span className={styles.emoji}>{city.emoji}</span>
+        <h3 className={styles.name}>{city.cityName}</h3>
+        <time className={styles.date}>({formatDate(city.date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </li>
     </>
   );
 }
