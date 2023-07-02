21ede5234e38e9da710ceef2ad0d702b48675a02
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-02 18:29:54 +0200

    feat: add a marker for each city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,23 +4,17 @@ import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import { useState } from "react";
 import "leaflet/dist/images/marker-shadow.png";
 import { Browser } from "leaflet";
+import { useCities } from "../context/CityContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   const [position, setPosition] = useState([37.1674282, -3.6116593]);
-  const successCallback = (position) => {
-    console.log(position);
-  };
-
-  const errorCallback = (error) => {
-    console.log(error);
-  };
-
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  console.log(cities);
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <MapContainer
@@ -33,11 +27,15 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => {
+          return (
+            <Marker key={city.cityName} position={city.position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
