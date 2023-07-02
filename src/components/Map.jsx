775383965f96b2f35b0e17af97204ff7ad664341
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-02 19:13:27 +0200

    feat: add mapCenter component to center map in a city when clicked

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,38 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, TileLayer, Popup, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
 import "leaflet/dist/images/marker-shadow.png";
 import { Browser } from "leaflet";
 import { useCities } from "../context/CityContext";
+import { useGeolocation } from "../hooks/useGeolocation";
 
 function Map() {
   const navigate = useNavigate();
   const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+  const [mapPosition, setMapPosition] = useState([37.1674282, -3.6116593]);
 
-  const [position, setPosition] = useState([37.1674282, -3.6116593]);
-  console.log(cities);
+  // const {
+  //   isLoading,
+  //   position: currPosition,
+  //   error,
+  //   getPosition,
+  // } = useGeolocation();
+
+  // useEffect(() => {
+  //   getPosition();
+  // }, []);
+  // console.log(currPosition);
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <MapContainer
-        center={position}
-        zoom={17}
+        center={mapPosition}
+        // center={[mapLat, mapLng]}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -37,9 +50,16 @@ function Map() {
             </Marker>
           );
         })}
+        <ChangeCenter position={[mapLat || 40, mapLng || 0]} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
