46a76f600f4c4be653b337719d0a941c61e2a63d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-07 23:12:13 +0200

    feat: add a Clear button in Message component when displaying an error

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,20 @@
+import Button from "./Button";
+import { useCities } from "../context/CityContext";
 import styles from "./Message.module.css";
 
 function Message({ message }) {
+  const { isError, dispatch } = useCities();
   return (
-    <p className={styles.message}>
-      <span role="img">👋</span> {message}
-    </p>
+    <>
+      <p className={styles.message}>
+        <span role="img">👋</span> {message}
+      </p>
+      {isError && (
+        <Button type="primary" onClick={() => dispatch({ type: "clearError" })}>
+          Clear
+        </Button>
+      )}
+    </>
   );
 }
 
