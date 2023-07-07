b12b8827392867f8f6ab325ded00bd474447cc83
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-07 17:13:50 +0200

    style: remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,6 @@ import { CityProvider } from "./context/CityContext";
 import { AuthProvider } from "./context/FakeAuthContext";
 import { Suspense, lazy } from "react";
 import ProtectedRoute from "./pages/ProtectedRoute";
-
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import Homepage from "./pages/Homepage";
-// import PageNotFound from "./pages/PageNotFound";
-// import AppLayout from "./pages/AppLayout";
-// import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
