a9fad670b53f591dc75f20ecd0a511ba815424ec
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-07 15:50:47 +0200

    feat: add deleteCity netlify function

---
+++ functions/deleteCity.js
@@ -0,0 +1,16 @@
+const FaunaService = require("@brianmmdev/faunaservice");
+
+exports.handler = async (event, contest) => {
+  const service = new FaunaService(process.env.FAUNA_API_KEY);
+
+  const id = event.queryStringParameters.id;
+  const city = await service.deleteRecord("Cities", id);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Authorization, Content-Type",
+    },
+  };
+};
