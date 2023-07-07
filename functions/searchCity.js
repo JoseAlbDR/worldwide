b02cf74715aaf90c5ba87872676ecf25aff52d31
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-07 23:09:38 +0200

    feat: handle 404 error in searchCity netlify function

--- functions/searchCity.js
+++ functions/searchCity.js
@@ -1,5 +1,4 @@
 exports.handler = async (event, contest) => {
-  console.log(process.env.OW_API_KEY);
   const cityName = event.queryStringParameters.name;
 
   const city = await fetch(
@@ -9,7 +8,23 @@ exports.handler = async (event, contest) => {
   // const city = await fetch(
   //   `http://api.openweathermap.org/geo/1.0/direct?q=London&appid=${process.env.OW_API_KEY}`
   // );
+
   const data = await city.json();
+
+  if (!data.length) {
+    return {
+      statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        error: `City ${cityName} not found, try another name.`,
+      }),
+    };
+  }
+
   const { lat } = data[0];
   const { lon: lng } = data[0];
   const coords = { lat, lng };
