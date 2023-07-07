4de3bcee8522b1243dc6589ca2d4ee4c70089597
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-07 15:15:15 +0200

    feat: cities function to list all cities in FaunaDB

--- functions/cities.js
+++ functions/cities.js
@@ -1,6 +1,16 @@
+const FaunaService = require("@brianmmdev/faunaservice");
+
 exports.handler = async (event, contest) => {
+  const service = new FaunaService(process.env.FAUNA_API_KEY);
+
+  const cities = await service.listRecords("Cities");
   return {
     statusCode: 200,
-    body: "hello world",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Authorization, Content-Type",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cities),
   };
 };
