16768a6a0f7c67837dfb8ea08786f7d49476bec6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-07 14:49:00 +0200

    feat: modify headers to get an ID

--- functions/addCity.js
+++ functions/addCity.js
@@ -5,12 +5,14 @@ exports.handler = async (event, contest) => {
 
   const city = JSON.parse(event.body);
 
-  await service.createRecord("Cities", city);
+  const cityCreated = await service.createRecord("Cities", city);
   return {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Authorization, Content-Type",
+      "Content-Type": "application/json",
     },
+    body: JSON.stringify(cityCreated),
   };
 };
